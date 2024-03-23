84aa139b96e63c241e78de1a086e59f2def77e0d
Luis Ruiz <lerb11@hotmail.com>
2024-03-23 00:21:14 -0500

    first commit

---
+++ src/test/java/exampleSpring/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package exampleSpring.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
